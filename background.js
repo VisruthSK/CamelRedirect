387e6a66df8db6861df56419711a002e276d3ff8
Vishwarrior26 <srimathkandali@gmail.com>
2021-12-17 23:08:56 -0800

    Got current tabs url, now to get the DP #

--- background.js
+++ background.js
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-function onCreated(tab) {
-  console.log(`Created new tab: ${tab.id}`)
-}
-
-function onGot(tabInfo) {
-  console.log(tabInfo);
-}
-
-function onError(error) {
-  console.log(`Error: ${error}`);
-}
-
-browser.browserAction.onClicked.addListener(function() {
-  var creating = browser.tabs.create({
-    url: "https://example.org"
-  });
-  // var test = currentWindow.getBrowser();
-  // console.log(test)
-  const gettingCurrent = browser.tabs.getCurrent();
-  gettingCurrent.then(onGot, onError);
-  creating.then(onCreated, onError);
-=======
 browser.browserAction.onClicked.addListener(function() {
   browser.tabs.query({
     active: true,
@@ -30,5 +7,4 @@ browser.browserAction.onClicked.addListener(function() {
     var activeTabUrl = activeTab.url;
     console.log(activeTabUrl)
   });
->>>>>>> bb4fef9e142745b189c4d8b5a72e6df4263be186
 });
