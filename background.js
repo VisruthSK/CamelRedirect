6eeb6c12eb6221b198a56d346b795652f70bae4f
Vishwarrior26 <srimathkandali@gmail.com>
2021-12-18 11:44:54 -0800

    Added support for other countries?

--- background.js
+++ background.js
@@ -5,22 +5,24 @@ browser.browserAction.onClicked.addListener(function() {
   }, function(tabs) {
     var activeTab = tabs[0];
     var activeTabUrl = activeTab.url;
-    console.log(activeTabUrl)
+    // console.log(activeTabUrl)
     //Add support for other countries from CCC website
-    let amazonLinks = ['https://www.amazon.com.au', 'https://www.amazon.com.ca', 'https://www.amazon.com.fr', 'https://www.amazon.com.de', 'https://www.amazon.com.it', 'https://www.amazon.com.es', 'https://www.amazon.com.uk', 'https://www.amazon.com']
-    if (activeTabUrl.slice(0, 23) == "https://www.amazon.com/") {
-      console.log('Amazon tab!')
-      const skuIndex = activeTabUrl.indexOf('/B0') // Maybe remove the 0? Change to regex later?
+    let amazonLinks = ['au', 'ca', 'fr', 'de', 'it', 'es', 'uk']
+    if (activeTabUrl.slice(0, 22) == "https://www.amazon.com") {
+      var ccTLD = amazonLinks.indexOf(activeTabUrl.slice(23, 25))
+      var country = ''
+      if (ccTLD != -1) {
+        country = amazonLinks[ccTLD] + '.'
+      }
+      const skuIndex = activeTabUrl.indexOf('/B0') // Change to regex later?
       var sku = activeTabUrl.slice(skuIndex, skuIndex + 11)
-      console.log(sku)
-      var final = "https://camelcamelcamel.com/product"
-      final = final.concat(sku)
+      var final = "https://" + country + "camelcamelcamel.com/product" + sku
       console.log(final)
       var creating = browser.tabs.create({
         url: final
       });
     } else {
-      console.log('Not Amazon tab')
+      // console.log('Not Amazon tab')
     }
 
   });
