010fd5519bcf62a236daf73c25c76a9a0a884749
Vishwarrior26 <srimathkandali@gmail.com>
2021-12-17 23:08:04 -0800

    Trying some stuff

--- background.js
+++ background.js
@@ -1,3 +1,22 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
-   chrome.tabs.executeScript(null, {file: "redirect.js"});
+function onCreated(tab) {
+  console.log(`Created new tab: ${tab.id}`)
+}
+
+function onGot(tabInfo) {
+  console.log(tabInfo);
+}
+
+function onError(error) {
+  console.log(`Error: ${error}`);
+}
+
+browser.browserAction.onClicked.addListener(function() {
+  var creating = browser.tabs.create({
+    url: "https://example.org"
+  });
+  // var test = currentWindow.getBrowser();
+  // console.log(test)
+  const gettingCurrent = browser.tabs.getCurrent();
+  gettingCurrent.then(onGot, onError);
+  creating.then(onCreated, onError);
 });
